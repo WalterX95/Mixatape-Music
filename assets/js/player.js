bf874602c6b2628f17d9a5b76375b92119db8a84
WalterX95 <walter_1995@hotmail.it>
2024-12-31 13:37:02 +0100

    Controllo Audio Muted e progress bar Youtube Inserito

--- assets/js/player.js
+++ assets/js/player.js
@@ -6,68 +6,101 @@ const trackingAudio = document.getElementById("trackingAudio");
 const volumBtn = document.getElementById('volumBtn');
 const mutedPlayer = document.getElementById('mutedPlayer');
 
-
-const objAudio = {
-    src:'./assets/music/song.mp3',
-    loop: true,
-    volume:1.0,
-    mute:false
-}
-
-const audio = new Audio(objAudio.src);
-
 let audioState = false;
 
+// 2. This code loads the IFrame Player API code asynchronously.
+let tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+let player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('YTPlayer', {
+        height: '60',
+        width: '60',
+        videoId: 'M7lc1UVf-VE',
+        events: {
+            'onStateChange': onPlayerStateChange,
+            'onPlayerStateChange': onPlayerStateChange
+        }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+let done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setInterval(audioTrackingData, 1000);
+        done = true;
+    }
+}
+function stopVideo() {
+    player.stopVideo();
+}
+
 const audioTrackingData = () => {
-    trackingAudio.value = audio.currentTime / audio.duration * 100;
+    trackingAudio.value = player.getCurrentTime() / player.getDuration() * 100;
 };
 
 const PlayStatePauseIcon = () => {
-   playSongBtn.classList.remove("bi-play-fill");
-   playSongBtn.classList.add("bi-pause-fill");
-   audio.play();
+    playSongBtn.classList.remove("bi-play-fill");
+    playSongBtn.classList.add("bi-pause-fill");
+    player.playVideo();
+
 };
 
 const PlayStateStartIcon = () => {
     playSongBtn.classList.remove("bi-pause-fill");
     playSongBtn.classList.add("bi-play-fill");
-    audio.pause();
+    stopVideo();
 };
 
 const VolumeChange = () => {
-    audio.volume = volumBtn.value / 100;
-    console.log(audio.volume);  
+    player.setVolume(volumBtn.step);
+    console.log(player.setVolume(volumBtn.step));
 };
 
 const toggleMute = () => {
-    if (audio.muted) {
-            audio.muted = false;
-            volumBtn.setAttribute('value', '100');
-            mutedPlayer.classList.remove('bi-volume-mute');
-            mutedPlayer.classList.add('bi-volume-up');
-    } 
+    if (player.isMuted) {
+        player.isMuted = false;
+        volumBtn.setAttribute('value', '100');
+        mutedPlayer.classList.remove('bi-volume-up');
+        mutedPlayer.classList.add('bi-volume-mute');
+        player.setVolume(100);
+
+    }
     else {
-            audio.muted = true;
-            volumBtn.setAttribute('value', '0');
-            mutedPlayer.classList.remove('bi-volume-up');
-            mutedPlayer.classList.add('bi-volume-mute');
+        player.isMuted = true;
+        volumBtn.setAttribute('value', '0');
+        mutedPlayer.classList.remove('bi-volume-mute');
+        mutedPlayer.classList.add('bi-volume-up');
+        player.setVolume(0);
     }
 };
 
 mutedPlayer.addEventListener("click", () => { toggleMute(); });
 
-volumBtn.addEventListener("input",() => { VolumeChange(); });
-//UpdateTime Song
-audio.addEventListener("timeupdate", () => { audioTrackingData(); });
- 
+volumBtn.addEventListener("input", () => { VolumeChange(); });
+
 playSongBtn.addEventListener("click", (e) => {
     e.preventDefault();
     audioState = !audioState;
     console.log(audioState);
-    if(audioState) {
+    if (audioState) {
         PlayStatePauseIcon();
     }
     else {
         PlayStateStartIcon();
     }
 });
+
